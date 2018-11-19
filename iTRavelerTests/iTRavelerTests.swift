9e4841a3bdae420190e9325f0bcfb2230b07b72c
Erandra Jayasundara <erandrajayasundara@live.com>
2018-11-19 10:09:54 +0530

    Initial Commit

---
+++ iTRavelerTests/iTRavelerTests.swift
@@ -0,0 +1,36 @@
+//
+//  iTRavelerTests.swift
+//  iTRavelerTests
+//
+//  Created by Erandra Jayasundara on 11/19/18.
+//  Copyright © 2018 Erandra Jayasundara. All rights reserved.
+//
+
+import XCTest
+@testable import iTRaveler
+
+class iTRavelerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
